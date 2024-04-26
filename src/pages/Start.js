eb486b1462ec73f071d5c0a4ae21cf7bc8315c38
해원 <haewon1106@gmail.com>
2024-04-26 15:12:51 +0900

    시작화면 구현

---
+++ src/pages/Start.js
@@ -0,0 +1,32 @@
+import React, { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { Icon } from '@iconify/react';
+import BackgroundImg from '../commponents/BackgroundImg';
+import styles from '../styles/Start.module.css'
+import GlassFull from '../commponents/GlassFull'
+
+function Start() {
+    const navigate = useNavigate();
+
+    const handleClick = (path) => {
+        navigate(path);
+      };
+
+    return (
+        <div>
+            <div className={styles['container']}>
+                <div className={styles['textContainer']}>
+                    <p className={styles['subText']}>To you in the future</p>
+                    <h1 className={styles['mainText']}>time capsule</h1>
+                </div>
+                <button className={styles['btnStyle']}>작성하기<Icon icon="formkit:right"></Icon></button>
+                <button className={styles['galleryBtn']}>갤러리 바로가기<Icon icon="ep:right"></Icon></button>
+            </div>
+            
+            <GlassFull />
+            <BackgroundImg />
+        </div>
+    );
+}
+
+export default Start;

1e8f215249a4d766d5bc350502df26a4438c3f93
해원 <haewon1106@gmail.com>
2024-04-26 15:11:03 +0900

    초기세팅

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import Start from './pages/Start';
+import Loading from './pages/Loading';
+import Send from './pages/Send'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<Start />} />
+                <Route path="/loading" element={<Loading />} />
+                <Route path="/send" element={<Send />} />
+            </Routes>
+        </Router>
+    )
 }
 
 export default App;
+

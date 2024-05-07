c55fa6aa19e6484b0b4e7a55c9f5d4a9eef51b2d
해원 <haewon1106@gmail.com>
2024-05-07 13:33:52 +0900

    [Fix] 페이지가 wirte일때만 전송 버튼

--- src/commponents/Glass.js
+++ src/commponents/Glass.js
@@ -1,14 +1,16 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Icon } from '@iconify/react';
+import { useLocation } from 'react-router-dom';
 import styles from '../styles/Glass.module.css'
 
 function Glass() {
-
+    const location = useLocation();
+    const currentPage = location.pathname;
     return (
         <div className={styles['glassContainer']}>
             <Icon icon="icon-park-solid:left-c" className={styles['btnStyles']}/>
             <div className={styles['glass']}></div>
-            <img src='/images/send.png' className={styles['btnStyles']}/>
+            {currentPage === '/Write' ? <img src='/images/send.png' className={styles['btnStyles']}/> : <Icon icon="icon-park-solid:right-c" className={styles['btnStyles']}/>}
         </div>
     );
 }

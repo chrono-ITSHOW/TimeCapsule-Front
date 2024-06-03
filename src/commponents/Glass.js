df464c157365c2cd375344e7cc94d351cd07b596
해원 <haewon1106@gmail.com>
2024-06-03 11:38:11 +0900

    Docs: 유리, 버튼 컴포넌트 분리

--- src/commponents/Glass.js
+++ src/commponents/Glass.js
@@ -1,35 +1,19 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Icon } from '@iconify/react';
 import { useLocation } from 'react-router-dom';
 import styles from '../styles/Glass.module.css';
-import Popup from './Popup';
 
 const Glass = () => {
-    const location = useLocation();
-    const currentPage = location.pathname;
-
-    const [isPopupOpen, setIsPopupOpen] = useState(false);
-
-    const handlePopupOpen = () => {
-        setIsPopupOpen(true);
-    };
-    return (
-        <div className={styles['glassContainer']}>
-            <Icon icon="icon-park-solid:left-c" className={styles['btnStyles']} />
-            <div className={styles['glass']}></div>
-            {currentPage === '/write' ? (
-                <img
-                    src='/images/send.png'
-                    className={styles['btnStyles']}
-                    alt='보내기'
-                    onClick={handlePopupOpen}
-                />
-            ) : (
-                <Icon icon="icon-park-solid:right-c" className={styles['btnStyles']} />
-            )}
-            {isPopupOpen && <Popup />}
-        </div>
-    );
+  const location = useLocation();
+  
+  return (
+    <div className={styles['container']}>
+      <img src='/images/timecapsule.png' className={styles['title']} alt="타임캡슐" />
+      <div className={styles['glassContainer']}>
+        <div className={styles['glass']}></div>
+      </div>
+    </div>
+  );
 }
 
 export default Glass;

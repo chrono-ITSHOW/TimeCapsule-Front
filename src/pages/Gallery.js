3202665fb6ba1abf5d3b8b0f6edb9fd57b23bc28
rlagkdms <ithaeun@naver.com>
2024-06-13 12:01:02 +0900

    style : 갤러리 화면 ui 구현

---
+++ src/pages/Gallery.js
@@ -0,0 +1,23 @@
+import React from "react";
+// import SphareItem from '../components/gallery/SphareItem'
+import { Canvas } from "@react-three/fiber";
+import BoxPhysics from "../components/gallery/BoxPhysics";
+
+const color = 0xffffff;
+const intensity = 1;
+
+function Gallery() {
+  // let itemList = [];
+  //   const addItem = () => {
+  //     for(var i=0; i<10; i++){
+  //       itemList.push(<SphareItem/>);
+  //     }
+  //   }
+
+  return (
+    <BoxPhysics />
+    // <div></div>
+  );
+}
+
+export default Gallery;

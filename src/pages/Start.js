7580a6e782f392fbefa520a5f921bb3079d49160
해원 <haewon1106@gmail.com>
2024-06-13 11:49:15 +0900

    Docs: 버튼 스타일 수정

--- src/pages/Start.js
+++ src/pages/Start.js
@@ -16,14 +16,11 @@ function Start() {
         <div>
             <div className={styles['container']}>
                 <div className={styles['textContainer']}>
-                    <p className={styles['subText']}>To you in the future</p>
-                    <h1 className={styles['mainText']}>time capsule</h1>
                 </div>
                 <button className={styles['btnStyle']} onClick={() => handleClick('/write')}>작성하기<Icon icon="formkit:right"></Icon></button>
-                <div className={styles['galleryBtn']} onClick={() => handleClick('/gallery')}>갤러리 바로가기<Icon icon="ep:right"></Icon></div>
+                <button className={styles['btnStyle']} onClick={() => handleClick('/gallery')}>갤러리 바로가기<Icon icon="formkit:right"></Icon></button>
             </div>
             
-            <GlassFull />
             <BackgroundImg />
         </div>
     );
